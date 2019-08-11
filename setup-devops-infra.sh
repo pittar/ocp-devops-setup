cb874ec67010ba12b1c143996caee344a587b943
Andrew Pitt <apitt@redhat.com>
2019-08-11 18:40:30 -0400

    Finally committing udpates.

--- setup-devops-infra.sh
+++ setup-devops-infra.sh
@@ -11,7 +11,11 @@ oc new-project app-qa --display-name="QA: App" --description="QA environment for
 echo "Created project QA: App."
 
 # Add a new project to keep service accounts for external tools such as Azure DevOps.
+# Not required in this demo, but leavnig this in here for demonstration purposes.
 # oc new-project serviceaccounts --display-name="Service Accounts" --description="Service accounts for external tools."
+# oc create sa azdevops -n serviceaccounts
+# oc policy add-role-to-user admin system:serviceaccount:serviceaccounts:azdevops -n app-dev
+# oc policy add-role-to-user admin system:serviceaccount:serviceaccounts:azdevops -n app-qa
 
 # Allow DEV to pull from CI/CD
 oc policy add-role-to-user system:image-puller system:serviceaccount:app-dev:default -n cicd
@@ -21,15 +25,26 @@ echo "Services in DEV can pull images from CI/CD."
 oc policy add-role-to-user system:image-puller system:serviceaccount:app-qa:default -n cicd
 echo "Services in QA can pull images from CI/CD."
 
+# Start Jenkins Persistent
+oc new-app openshift/jenkins-persistent -n cicd
+echo "Launching Jenkins."
+
 # Spin up DevOps tools.
 # Using Nexus 2 simply because it takes fewer resources.
-oc process -n cicd -f https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-persistent-template.yaml | oc create -n cicd -f -
+oc new-app -f https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-persistent-template.yaml -n cicd
+echo "Launching Nexus 2."
 
 # SonarQube.
 oc new-app -f https://raw.githubusercontent.com/pittar/sonarqube-openshift-docker/master/sonarqube-postgresql-template.yaml --param=SONARQUBE_VERSION=7.0 -n cicd
+echo "Launching SonarQube."
 
 # Dependency Track.
 #oc new-app -f https://raw.githubusercontent.com/pittar/openshift-dependency-track/master/dependency-track.yaml -n cicd
+
+# Grant Jenkins service account access to dev and qa projects.
+oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n app-dev
+oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n app-qa
+echo "Jenkins granted admin on DEV and QA projects."
 
 # Add build and app templates to cicd.
 oc apply -f resources/build-template.yaml -n cicd
@@ -37,24 +52,13 @@ oc apply -f resources/backend-template.yaml -n cicd
 oc apply -f resources/frontend-template.yaml -n cicd
 echo "Added build template and app template."
 
-# Create app deployments in DEV and QA.
-#oc process demo-app-template -n cicd | oc create -n app-dev -f -
-#oc process demo-app-template -n cicd | oc create -n app-qa -f -
-#echo "Created deployments.  Wait 5 seconds then cancel since there are no images yet."
-#sleep 5
-#oc rollout cancel dc/backend -n app-dev && oc rollout cancel dc/frontend -n app-dev
-#oc rollout cancel dc/backend -n app-qa && oc rollout cancel dc/frontend -n app-qa
-#echo "Instantiated app in DEV and QA."
-
 # Create the frontend and backend builds.
-oc process jenkins-pipeline -p APP_NAME=frontend -p GIT_SOURCE_URL=https://github.com/pittar/springboot-frontend -n cicd | oc create -n cicd -f -
-oc process jenkins-pipeline -p APP_NAME=backend -p GIT_SOURCE_URL=https://github.com/pittar/springboot-backend -n cicd | oc create -n cicd -f -
+oc new-app cicd/jenkins-pipeline -p APP_NAME=frontend -p GIT_SOURCE_URL=https://github.com/pittar/springboot-frontend -n cicd
+oc new-app cicd/jenkins-pipeline -p APP_NAME=backend -p GIT_SOURCE_URL=https://github.com/pittar/springboot-backend -n cicd
+echo "Created frontend and backend builds and pipelines."
 
-# Grant Jenkins service account access to dev and qa projects.
-oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n app-dev
-oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n app-qa
-
-# Create ImageStreams for the frontend and backend images.
+# Create ImageStreams for the frontend and backend images. 
+# DON'T DO THIS IF BUILDING THE IMAGES FROM SOURCE! 
 # oc tag docker.io/pittar/springboot-backend:latest springboot-backend:latest --scheduled=true -n cicd
 # oc tag docker.io/pittar/springboot-frontend:latest springboot-frontend:latest --scheduled=true -n cicd
 
